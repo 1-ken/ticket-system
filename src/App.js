df9619d8eb0ce0a6a3e9ee9489308088b617f428
1-ken <kennicmwimu@gmail.com>
2025-05-09 06:38:33 +0300

    bsasic routes added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
+import ForgotPassword from "./pages/ForgotPassword";
+import { BrowserRouter as Router,Route,Routes } from "react-router-dom";
 function App() {
   return (
     <>
-    <h1 className="font-bold underline">home</h1>
+      <Router>
+        <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/profile" element={<Profile/>}/>
+        <Route path="/sign-in" element={<Signin/>}/>
+        <Route path="/sign-up" element={<Signup/>}/>
+        <Route path="/forgot-password" element={<ForgotPassword/>}/>
+        </Routes>
+      </Router>
     </>
   );
 }
 
-export default App;
+export default App; 
